54e8cdce132aeedde0a3c0975b0c73562f9868a5
raufur <raufurshekh45@gmail.com>
2026-05-17 10:42:04 +0600

    req.params and req.query

--- express/app.js
+++ express/app.js
@@ -10,6 +10,83 @@ app.get("/",(req,res)=>{
 app.get("/about",(req,res)=>{
     return res.end("This is about page with the help of express js")
 })
+
+//req.params
+let employee = [
+  {
+    "id": 1,
+    "name": "John Doe",
+    "age": 29,
+    "department": "Engineering",
+    "position": "Software Developer",
+    "salary": 65000,
+    "email": "john.doe@example.com",
+    "isActive": true
+  },
+  {
+    "id": 2,
+    "name": "Sarah Smith",
+    "age": 34,
+    "department": "Human Resources",
+    "position": "HR Manager",
+    "salary": 72000,
+    "email": "sarah.smith@example.com",
+    "isActive": true
+  },
+  {
+    "id": 3,
+    "name": "Michael Johnson",
+    "age": 27,
+    "department": "Marketing",
+    "position": "Marketing Executive",
+    "salary": 54000,
+    "email": "michael.johnson@example.com",
+    "isActive": false
+  },
+  {
+    "id": 4,
+    "name": "Emily Davis",
+    "age": 31,
+    "department": "Finance",
+    "position": "Financial Analyst",
+    "salary": 68000,
+    "email": "emily.davis@example.com",
+    "isActive": true
+  },
+  {
+    "id": 5,
+    "name": "David Wilson",
+    "age": 38,
+    "department": "Sales",
+    "position": "Sales Manager",
+    "salary": 75000,
+    "email": "david.wilson@example.com",
+    "isActive": true
+  }
+]
+app.get("/employee",(req,res)=>{
+    return res.json(employee)
+})
+app.get("/employee/:id",(req , res)=>{
+    let id = req.params.id
+    let existUser = employee.find((employee)=>employee.id == id)
+    if(!existUser){
+          console.log(`No such employee of ${id}`)
+        return res.end(`No such employee of ${id}`)
+    }
+    res.json(existUser)
+})
+
+
+//req.quuery 
+app.get("/search",(req , res)=>{
+    let query = req.query
+    console.log(query);
+    
+     return res.end('this is query peramiter page ')
+    
+     
+})
 //server is listing or creating first server
 app.listen(PORT,()=>{
     console.log(`server is running at the ${PORT}`);
